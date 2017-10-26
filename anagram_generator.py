7f9478660cc2a3006e38c4a19d59aca4d9e5026f
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-10-26 14:09:40 -0700

    fixed bug

--- anagram_generator.py
+++ anagram_generator.py
@@ -8,17 +8,27 @@ with open("/usr/share/dict/words", 'r') as f:
         clean = line.replace('\n', '')
         wordsList.append(clean)
 
+
 def anagram_generator():
+    find = False
     random_number = random.randint(0, len(wordsList) - 1)
     selected_word = wordsList[random_number]
-    print(selected_word)
+    original = selected_word
     number_of_characters = len(selected_word)
     anagram = ""
-    for i in range(0, number_of_characters):
-        number = random.randint(0, len(selected_word) - 1)
-        character = selected_word[number]
-        anagram += character
-        selected_word = selected_word[:number] + selected_word[number + 1:]
+    while find is False:
+        selected_word = original
+        anagram = ""
+        for i in range(0, number_of_characters):
+            number = random.randint(0, len(selected_word) - 1)
+            character = selected_word[number]
+            anagram += character
+            selected_word = selected_word[:number] + selected_word[number + 1:]
+        if anagram != original:
+            find = True
+        else:
+            find = False
+    print(original)
     return anagram
 
 if __name__ == '__main__':
