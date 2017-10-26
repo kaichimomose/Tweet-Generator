26385fa8eb5012fa8422fd32d62aedcf924df49e
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-10-26 12:50:43 -0700

    solve anagram challenge

---
+++ anagram_generator.py
@@ -0,0 +1,26 @@
+import random
+random.seed(1)
+
+wordsList = []
+
+with open("/usr/share/dict/words", 'r') as f:
+    for line in f:
+        clean = line.replace('\n', '')
+        wordsList.append(clean)
+
+def anagram_generator():
+    random_number = random.randint(0, len(wordsList) - 1)
+    selected_word = wordsList[random_number]
+    print(selected_word)
+    number_of_characters = len(selected_word)
+    anagram = ""
+    for i in range(0, number_of_characters):
+        number = random.randint(0, len(selected_word) - 1)
+        character = selected_word[number]
+        anagram += character
+        selected_word = selected_word[:number] + selected_word[number + 1:]
+    return anagram
+
+if __name__ == '__main__':
+    anagram = anagram_generator()
+    print(anagram)
