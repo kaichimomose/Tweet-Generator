4faa9131b92cc4fdf593460e9e0d3cbdc65928f5
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2018-01-06 17:08:08 -0800

    fixing bug

--- app.py
+++ app.py
@@ -1,43 +1,43 @@
 from flask import Flask, render_template, request, redirect
-from dictogram import Dictogram
-import list_counts
+# from dictogram import Dictogram
+# import list_counts
 import sentence
-import sample
-import cleanup
-# import twitter
-# import pdb
-# from second_order_markov_chain import Second_Marcov
-# import tokenizing
+# import sample
+# import cleanup
+import twitter
+import pdb
+from second_order_markov_chain import Second_Marcov
+import tokenizing
 
 app = Flask(__name__)
-file_name = "source-text/1661.txt"  # "corpus.txt"
-word = "the"
-word_list = cleanup.clean_up_words_from_file(file_name)
-# source = open(file_name).read()
-# word_list = tokenizing.tokenize(source)
-# second_markov = Second_Marcov(word_list)
+file_name = "corpus.txt"  # "source-text/1661.txt"
+# word = "the"
+# word_list = cleanup.clean_up_words_from_file(file_name)
+source = open(file_name).read()
+word_list = tokenizing.tokenize(source)
+second_markov = Second_Marcov(word_list)
 
 @app.route('/')
 def generate_sentence():
-    number_of_words = request.args.get('num', default=10, type=int)
-    histogram = Dictogram(word_list)
-    histogram_list_counts = list_counts.make_list_counts(histogram)
-    picked_words = sample.pick_many_words(histogram.tokens, histogram_list_counts, number_of_words)
-    made_sentence = sentence.make_sentence(picked_words)
-    # picked_words = second_markov.pick_many_words()
+    # number_of_words = request.args.get('num', default=10, type=int)
+    # histogram = Dictogram(word_list)
+    # histogram_list_counts = list_counts.make_list_counts(histogram)
+    # picked_words = sample.pick_many_words(histogram.tokens, histogram_list_counts, number_of_words)
     # made_sentence = sentence.make_sentence(picked_words)
+    picked_words = second_markov.pick_many_words()
+    made_sentence = sentence.make_sentence(picked_words)
     return render_template("home.html", something=made_sentence)
 
 
 @app.route('/newsentence')
 def generate_new_sentence():
-    number_of_words = request.args.get('num', default=10, type=int)
-    histogram = Dictogram(word_list)
-    histogram_list_counts = list_counts.make_list_counts(histogram)
-    picked_words = sample.pick_many_words(histogram.tokens, histogram_list_counts, number_of_words)
-    made_sentence = sentence.make_sentence(picked_words)
-    # picked_words = second_markov.pick_many_words()
+    # number_of_words = request.args.get('num', default=10, type=int)
+    # histogram = Dictogram(word_list)
+    # histogram_list_counts = list_counts.make_list_counts(histogram)
+    # picked_words = sample.pick_many_words(histogram.tokens, histogram_list_counts, number_of_words)
     # made_sentence = sentence.make_sentence(picked_words)
+    picked_words = second_markov.pick_many_words()
+    made_sentence = sentence.make_sentence(picked_words)
     return render_template("home.html", something=made_sentence)
 
 
